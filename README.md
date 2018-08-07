6ec425630be95b8712f67c1f3437e55ee2da91c3
Justin Littman <justinlittman@gwu.edu>
2018-08-07 11:45:44 -0400

    Added additional subject and keywords. Moved subject and keywords into separate file. Added asking about extracts. Collects keywords without subjects and vice-versa.

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # vopd
 Code supporting the Voices of Demonization and Polarization project
-
-## TODO
-1. Add additional subject and keywords.
-2. Give codes to subject and keywords.
-3. Split parties, groups, individuals.
-4. Ask user n or y. Highlight terms in context.
-5. Also write no's to separate file
-6. Also write subjects that appear without keywords, and keywords that appear without subjects.
--- vopd.py
+++ vopd.py
@@ -7,44 +7,14 @@ import csv
 import re
 from nltk.tokenize import word_tokenize
 import nltk
+from normalize_terms import normalize_terms
+from subjects import subject_map
+from keywords import keyword_map
 
 nltk.download('punkt')
 
-subjects = [
-    'kanye',
-    'boehner',
-    'obama',
-    'holder',
-    'pruitt',
-    'trump',
-    'sanders',
-    'soros',
-    'clinton',
-    'kushner',
-    'samantha_bee'
-]
-
-keywords = [
-    'corrupt',
-    'corrupting',
-    'corruption',
-    'desperate',
-    'fight',
-    'evil',
-    'rat',
-    'attack',
-    'attacking',
-    'criminal',
-    'crazy',
-    'scoundrel',
-    'phony',
-    'treason',
-    'cheat'
-]
-
-normalize_terms = [
-    ('samantha bee', 'samantha_bee')
-]
+subjects = list(subject_map.keys())
+keywords = list(keyword_map.keys())
 
 
 def extract_text(pdf_filepath):
@@ -60,7 +30,7 @@ def tokenize(transcript_text):
     # Split words by periods
     clean_transcript_text = re.sub(r'([a-z])\.([a-z])', r'\1. \2', clean_transcript_text)
 
-    for term, normalized_term in normalize_terms:
+    for term, normalized_term in normalize_terms.items():
         clean_transcript_text = clean_transcript_text.replace(term, normalized_term)
     return word_tokenize(clean_transcript_text)
 
@@ -99,13 +69,26 @@ def process_transcript_iter(transcript_words, window_size=10):
         if subject_pos is not None:
             keyword_pos, keyword = matching_word_list(window_words, keywords)
             if keyword_pos is not None:
+                # A subject and keyword found
                 yield subject, start + subject_pos, keyword, start + keyword_pos
+            else:
+                # Only a subject found
+                yield subject, start + subject_pos, None, None
+        else:
+            # Look for keyword without a subject
+            keyword_pos, keyword = matching_word_list(window_words[0:1], keywords)
+            if keyword_pos is not None:
+                subject_pos, subject = matching_word_list(window_words, subjects)
+                if subject_pos is None:
+                    yield None, None, keyword, start + keyword_pos
+
     # Look backwards
     for start, end, window_words in back_window_iter(transcript_words, window_size):
         subject_pos, subject = matching_word_list(window_words[len(window_words) - 1:], subjects)
         if subject_pos is not None:
             keyword_pos, keyword = matching_word_list(window_words, keywords)
             if keyword_pos is not None:
+                # A subject and keyword found
                 yield subject, start + len(window_words) - 1, keyword, start + keyword_pos
 
 
@@ -115,6 +98,7 @@ if __name__ == '__main__':
                         default=10)
     parser.add_argument('--context', help='number of words before and after subject and keyword to extract', type=int,
                         default=10)
+    parser.add_argument('--ask', action='store_true', help='Ask about each extract')
     parser.add_argument('transcript', help='filepath to transcript pdf or directory')
 
     args = parser.parse_args()
@@ -130,15 +114,60 @@ if __name__ == '__main__':
     else:
         m_transcript_filepaths.append(args.transcript)
 
-    with open('extracts.csv', 'w') as extract_file:
+    headers = ['file', 'subject', 'subject_code', 'keyword', 'keyword_code', 'extract']
+
+    with open('extracts.csv', 'w') as extract_file, \
+            open('neg_extracts.csv', 'w') as neg_extract_file, \
+            open('keyword_extracts.csv', 'w') as keyword_extract_file, \
+            open('subject_extracts.csv', 'w') as subject_extract_file:
         extract_csv = csv.writer(extract_file)
-        extract_csv.writerow(['file', 'subject', 'keyword', 'extract'])
+        extract_csv.writerow(headers)
+        neg_extract_csv = csv.writer(neg_extract_file)
+        neg_extract_csv.writerow(headers)
+        keyword_extract_csv = csv.writer(keyword_extract_file)
+        keyword_extract_csv.writerow(['file', 'keyword', 'keyword_code', 'extract'])
+        subject_extract_csv = csv.writer(subject_extract_file)
+        subject_extract_csv.writerow(['file', 'subject', 'subject_code', 'extract'])
+
         for m_transcript_filepath in m_transcript_filepaths:
             print('Processing {}'.format(m_transcript_filepath))
             m_transcript_text = extract_text(m_transcript_filepath)
             m_transcript_words = tokenize(m_transcript_text)
             for m_subject, m_subject_pos, m_keyword, m_keyword_pos in process_transcript_iter(m_transcript_words,
                                                                                               window_size=args.window):
-                extract_csv.writerow([m_transcript_filepath, m_subject, m_keyword, ' '.join(
-                    context(m_transcript_words, min(m_subject_pos, m_keyword_pos), max(m_subject_pos, m_keyword_pos),
-                            context_size=args.context))])
+                if m_keyword is None:
+                    extract = ' '.join(
+                        context(m_transcript_words, m_subject_pos,
+                                m_subject_pos,
+                                context_size=args.context))
+                    subject_extract_csv.writerow([m_transcript_filepath,
+                                                  m_subject,
+                                                  subject_map[m_subject],
+                                                  extract])
+                elif m_subject is None:
+                    extract = ' '.join(
+                        context(m_transcript_words, m_keyword_pos,
+                                m_keyword_pos,
+                                context_size=args.context))
+                    keyword_extract_csv.writerow([m_transcript_filepath,
+                                                  m_keyword,
+                                                  keyword_map[m_keyword],
+                                                  extract])
+                else:
+                    extract = ' '.join(
+                        context(m_transcript_words, min(m_subject_pos, m_keyword_pos),
+                                max(m_subject_pos, m_keyword_pos),
+                                context_size=args.context))
+                    cur_extract_csv = extract_csv
+
+                    if args.ask:
+                        res = input('{} ({} -> {}) [Yn] '.format(extract, m_subject, m_keyword))
+                        if res in ('n', 'N'):
+                            cur_extract_csv = neg_extract_csv
+
+                    cur_extract_csv.writerow([m_transcript_filepath,
+                                              m_subject,
+                                              subject_map[m_subject],
+                                              m_keyword,
+                                              keyword_map[m_keyword],
+                                              extract])
